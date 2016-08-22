23874fdd4f3adb11be07ad4ff99dd68048f6ca39
jjunCoder <jjune21@gmail.com>
2016-08-22 11:42:19 +0900

    n 찍기를 했는데 시간초과가 났었음... cout 말고 prinf를 많이 쓰자!

---
+++ 2741/2741/main.cpp
@@ -0,0 +1,25 @@
+/*
+2016.8.22
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#2741
+N찍기
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		//cout 으로 했더니 시간초과남..
+		printf("%d\n", i);
+	}
+	return 0;
+}
