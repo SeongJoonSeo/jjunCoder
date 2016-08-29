c65209d1e80552000abc203e44967e3aec47366a
jjunCoder <jjune21@gmail.com>
2016-08-29 14:38:01 +0900

    이항계수 단계 Clear , 동적 계획법 기초 시작. ( 이항 계수 단계 이전 것들도 함께 커밋.)

---
+++ 2748/2748/소스.cpp
@@ -0,0 +1,29 @@
+﻿/*
+2016.8.26
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#2748
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	vector<long long> v(91, 0);
+	int n;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	v[0] = 0;
+	v[1] = 1;
+	for (int i = 2; i <= 90; i++) {
+		v[i] = v[i - 1] + v[i - 2];
+	}
+	cout << v[n];
+	return 0;
+}
