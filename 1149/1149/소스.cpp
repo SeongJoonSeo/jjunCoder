8a92931d26cf67aef49995a4e9c6e5faf6b7edaf
jjunCoder <jjune21@gmail.com>
2016-08-29 17:20:26 +0900

    RGB 거리 문제 solve

--- 1149/1149/소스.cpp
+++ 1149/1149/소스.cpp
@@ -16,41 +16,30 @@ RGB거리
 
 using namespace std;
 
-vector<vector<int> > cost;
-vector<int> dp;
-
-int solve(int index, int prev) {
-	int mini = 987654321;
-	int& ret = dp[index];
-	//base case
-	if (index == 0) {
-		for (int i = 0; i < 3; i++)
-			if (i != prev)
-				mini = min(mini, cost[index][i]);
-		return ret = mini;
-	}
-	if (ret != -1)
-		return ret;	
-	for (int i = 0; i < 3; i++) {
-		if (i != prev)
-			mini = min(mini, solve(index - 1, i) + cost[index][i]);
-	}
-	return ret = mini;
-}
+vector<vector<int> > cost, dp;
 
 int main() {
 	int n, c;
 	cin.sync_with_stdio(false);
 	cin >> n;
 	cost.assign(n, vector<int>(3, 0));
-	dp.assign(n, -1);
+	dp.assign(n, vector<int>(3, 987654321));
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < 3; j++) {
 			cin >> c;
 			cost[i][j] = c;
 		}
 	}
-	solve(n - 1, -1);
-	cout << dp[n - 1];
+	for (int i = 0; i < 3; i++)
+		dp[0][i] = cost[0][i];
+	for (int index = 1; index < n; index++)
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				if (i != j)
+					dp[index][i] = min(dp[index][i], dp[index - 1][j] + cost[index][i]);
+	int ret = 987654321;
+	for (int i = 0; i < 3; i++)
+		ret = min(ret, dp[n - 1][i]);
+	cout << ret;
 	return 0;
 }
