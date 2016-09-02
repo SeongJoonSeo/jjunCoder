642ff0932d5b140a913ec5213ef97c1f0a58bb32
jjunCoder <jjune21@gmail.com>
2016-09-02 09:57:17 +0900

    2579 계단 오르기 클리어

--- 2579/2579/소스.cpp
+++ 2579/2579/소스.cpp
@@ -17,41 +17,26 @@ Problem
 using namespace std;
 
 vector<int> stair;
-vector<int> dp;
+vector<vector<int> > dp;
 
-int solve(int n,vector<bool> step) {
-	if (n < 0)
-		return 0;
-	if (n == 0)
-		return stair[0];
-	int& ret = dp[n];
+int solve(int n,int step) {
+	if (n < 0) return 0;
+	int& ret = dp[n][step];
 	if (ret != -1) return ret;
-	step[n] = true;
-	if (n >= 2 && (step[n - 2] && step[n - 1] && step[n]))
-		return 0;
-	if (n < step.size() - 1 && (step[n - 1] && step[n] && step[n + 1]))
-		return 0;
-	if (n < step.size() - 2 && (step[n] && step[n + 1] && step[n + 2]))
-		return 0;
-	int maxi = -1;
-	if (n == step.size() - 1)
-		maxi = solve(n - 1, step) + stair[n];
-	else if (!step[n + 1]) 
-		maxi = solve(n - 1, step) + stair[n];
-	return ret = max(maxi, solve(n - 2, step) + stair[n]);
+	if (n == 0) return ret = stair[0];
+	if (step == 1) ret = max(ret, stair[n] + solve(n - 1, 0));
+	return ret = max(ret, stair[n] + solve(n - 2, 1));	
 }
 
 int main() {	
 	int n, cost;
 	cin.sync_with_stdio(false);
 	cin >> n;
-	dp.assign(n, -1);
-	vector<bool> step(n, false);
+	dp.assign(n, vector<int>(2, -1));
 	for (int i = 0; i < n; i++) {
 		cin >> cost;
 		stair.push_back(cost);
 	}
-	dp[0] = stair[0];
-	cout << solve(n - 1, step);
+	cout << solve(n - 1, 1);
 	return 0;
 }
