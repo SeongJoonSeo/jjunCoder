31acc967168c67c26b20504bdb7bcd0477a9944d
jjunCoder <jjune21@gmail.com>
2016-09-02 13:30:47 +0900

    1309 동물원 solve

---
+++ 1309/1309/소스.cpp
@@ -0,0 +1,28 @@
+﻿/*
+2016.9.2
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1309
+동물원
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	vector<int> dp(n + 1, 0);
+	dp[0] = 1;
+	dp[1] = 3;
+	for (int i = 2; i <= n; i++)
+		dp[i] = (2 * dp[i - 1] + dp[i - 2]) % 9901;
+	cout << dp[n];
+	return 0;
+}
