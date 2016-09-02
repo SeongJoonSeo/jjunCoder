346d872be91b21e8f734297b3b09497360e296ad
jjunCoder <jjune21@gmail.com>
2016-09-02 13:52:47 +0900

    오르막 수 solve

---
+++ 11057/11057/소스.cpp
@@ -0,0 +1,35 @@
+﻿/*
+2016.9.2
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#11057
+오르막 수
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	vector<vector<int> > dp;
+	dp.assign(n, vector<int>(10, 0));
+	for (int i = 0; i < 10; i++) {
+		dp[0][i] = i+1;
+	}
+	for (int j = 1; j < n; j++) {
+		for (int i = 0; i < 10; i++) {
+			if (i == 0) dp[j][i] = 1;
+			else dp[j][i] = (dp[j][i - 1] + dp[j - 1][i]) % 10007;
+		}
+	}
+	cout << dp[n - 1][9];
+	return 0;
+}
