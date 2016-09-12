3d7cae3886b3503078d14f2acf927a83b10d6a0e
jjunCoder <jjune21@gmail.com>
2016-09-13 08:52:21 +0900

    10926 번 문제인 ??! 는 c++ 의 '삼중자' 라는 것에 대하여 아느냐? 인 문제였다. ??!를 그대로 사용할 시에 | 로 변환된다. 그러므로 ?\?! 처럼 이스케이프 문자를 이용하여서 문제를 풀어야 해결이 된다.

---
+++ 10926/10926/소스.cpp
@@ -0,0 +1,23 @@
+﻿/*
+2016.9.13
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#10926
+??!
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string input;
+	getline(cin, input);
+	cout << input << "?\?!";
+	return 0;
+}
