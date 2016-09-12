3578771a3b6022a0f83c80d3e9725a1fefe389a9
jjunCoder <jjune21@gmail.com>
2016-09-12 13:05:14 +0900

    기본적인 벡터 문제였다.

---
+++ 10810/10810/소스.cpp
@@ -0,0 +1,34 @@
+﻿/*
+2016.9.12
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#10810
+공 넣기
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+int main() {
+	int n, m, i, j, k;
+	cin.sync_with_stdio(false);
+	cin >> n >> m;
+	vector<int> bucket(n+1,0);
+	for (int a = 0; a < m; a++) {
+		cin >> i >> j >> k;
+		for (int b = i; b <= j; b++) {
+			bucket[b] = k;
+		}
+	}
+	for (auto it = bucket.begin()+1; it != bucket.end(); it++)
+		printf("%d ", *it);
+	return 0;
+}
