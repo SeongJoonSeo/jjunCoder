6aedc3b6b747c653372c5fadf5af7c617eecff01
jjunCoder <jjune21@gmail.com>
2016-09-12 11:47:47 +0900

    set 은 삽입하면 알아서 정렬이 된다.

---
+++ 10867/10867/소스.cpp
@@ -0,0 +1,34 @@
+﻿/*
+2016.9.12
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#10867
+중복 빼고 정렬하기
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int main() {
+	int n, num;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	set<int> s;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		s.insert(num);
+	}
+	
+	for (auto it = s.begin(); it != s.end(); it++) {
+		printf("%d ", *it);
+	}
+	return 0;
+}
