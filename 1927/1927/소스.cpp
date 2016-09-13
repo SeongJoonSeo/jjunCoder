5555ad3c6e1e921dec53d83d3770094254bca81e
jjunCoder <jjune21@gmail.com>
2016-09-13 12:50:35 +0900

    minHeap 은 priority queue 에서 greater 조건을 넣어서 구현한다.

---
+++ 1927/1927/소스.cpp
@@ -0,0 +1,42 @@
+﻿/*
+2016.
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1927
+최소 힙
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <functional>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	int x;
+	priority_queue<int,vector<int>,greater<int> > minHeap;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		if (x == 0) {
+			if (minHeap.empty())
+				printf("%d\n",0);
+			else {
+				printf("%d\n", minHeap.top());
+				minHeap.pop();
+			}
+		}
+		else {
+			minHeap.push(x);
+		}
+	}
+	return 0;
+}
