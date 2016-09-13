072940850ddfe46f1891bb81d6ade5792c0dc7ba
jjunCoder <jjune21@gmail.com>
2016-09-13 13:27:08 +0900

    vector 안에 pair넣고 그 안에 pair 넣은 것을 sorting하는 문제...

---
+++ 10814/10814/소스.cpp
@@ -0,0 +1,37 @@
+﻿/*
+2016.9.13
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#10814
+나이순 정렬
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+	int n;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	vector<pair<int, pair<int, string> > > v;
+	for (int i = 0; i < n; i++) {
+		int age;
+		string name;
+		cin >> age >> name;
+		v.push_back(make_pair(age, make_pair(i, name)));
+	}
+	sort(v.begin(), v.end());
+	for (auto it = v.begin(); it != v.end(); it++) {
+		printf("%d ", (*it).first);
+		printf("%s\n", (*it).second.second.c_str());
+	}
+		
+	return 0;
+}
