68e8b7f851833c3d5090987ada68a4166c964486
jjunCoder <jjune21@gmail.com>
2016-09-13 12:43:28 +0900

    좌표 정렬하기 쉽게 solve

---
+++ 11650/11650/소스.cpp
@@ -0,0 +1,33 @@
+﻿/*
+2016.9.13
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#11650
+좌표 정렬하기
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, x, y;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	vector<pair<int, int> > v;
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		v.push_back(make_pair(x, y));
+	}
+	sort(v.begin(), v.end());
+	for (auto it = v.begin(); it != v.end(); it++) {
+		printf("%d ", (*it).first);
+		printf("%d\n", (*it).second);
+	}
+	return 0;
+}
