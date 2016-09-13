85cc41b8736420bad68c9b4002b7c0bf897a2708
jjunCoder <jjune21@gmail.com>
2016-09-13 13:44:59 +0900

    map의 second 는 ++ 연산자 등으로 수정 하는게 되더라.

---
+++ 11652/11652/소스.cpp
@@ -0,0 +1,48 @@
+﻿/*
+2016.9.13
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#11652
+카드
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	long long num;
+	map<long long, int> m;
+	int maxi = -1;
+	long long ret;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		auto it = m.find(num);
+		if (it != m.end()) {
+			(*it).second++;
+			if (maxi < (*it).second) {
+				maxi = (*it).second;
+				ret = (*it).first;
+			}
+			else if (maxi == (*it).second) {
+				maxi = (*it).second;
+				ret = min(ret, (*it).first);
+			}
+		}
+		else {
+			m.insert(make_pair(num, 0));
+		}
+	}
+	cout << ret;
+	return 0;
+}
