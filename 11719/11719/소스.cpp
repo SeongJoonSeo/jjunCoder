3a545ca4bf17a370bb35e961747dd7ca8575c694
jjunCoder <jjune21@gmail.com>
2016-09-13 11:34:35 +0900

    거지같은 Hex code 문제...

---
+++ 11719/11719/소스.cpp
@@ -0,0 +1,18 @@
+﻿/*
+2016.9.13
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#11719
+그대로 출력하기 2
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+	for (char a; ~scanf("%c", &a); printf("%c", a));
+	return 0;
+}
---
+++ 11784/11784/소스.cpp
@@ -0,0 +1,38 @@
+﻿/*
+2016.9.13
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#11784
+Hex Code
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string input;
+	getline(cin, input);
+	while(!cin.eof()) {		
+		int temp;
+		for (int i = 0; i < input.size(); i += 2) {
+			if (input[i] >= 65 && input[i] <= 70)
+				temp = (input[i] - 55) * 16;
+			else
+				temp = (input[i] - 48) * 16;
+			if (input[i + 1] >= 65 && input[i + 1] <= 70)
+				temp += (input[i + 1] - 55);
+			else
+				temp += (input[i + 1] - 48);
+			printf("%c", static_cast<char>(temp));
+		}
+		printf("\n");
+		getline(cin, input);
+	}	
+	return 0;
+}
