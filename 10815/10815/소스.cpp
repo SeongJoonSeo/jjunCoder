5bd992a2f11a86830c7e5a294e3562e5df16d8bc
jjunCoder <jjune21@gmail.com>
2016-09-13 13:18:37 +0900

    단순한 set 사용 문제였다. solve

---
+++ 10815/10815/소스.cpp
@@ -0,0 +1,38 @@
+﻿/*
+2016.9.13
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#10815
+숫자 카드
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int main() {
+	int n, num, m;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	set<int> s;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		s.insert(num);
+	}
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> num;
+		if (s.find(num) != s.end()) 
+			printf("%d ", 1);
+		else
+			printf("%d ", 0);		
+	}
+	return 0;
+}
