e2165cfcc09e500790d9836009eb00e023a2fd19
jjunCoder <jjune21@gmail.com>
2016-09-13 12:46:00 +0900

    좌표 정렬하기 2는 1과 그냥 똑같았다. solve

---
+++ 11651/11651/소스.cpp
@@ -0,0 +1,35 @@
+﻿/*
+2016.9.13
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#11651
+좌표 정렬하기 2
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	int x, y;
+	vector<pair<int, int> > v;
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		v.push_back(make_pair(y, x));
+	}
+	sort(v.begin(), v.end());
+	for (auto it = v.begin(); it != v.end(); it++) {
+		printf("%d ", (*it).second);
+		printf("%d\n", (*it).first);
+	}
+	return 0;
+}
