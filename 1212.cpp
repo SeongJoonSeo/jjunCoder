4d51a6dddf5e9ee0e9ab7356e92497022dd5d2db
SeongJoonSeo <jjune21@naver.com>
2016-09-19 20:00:19 +0900

    2526번 solve.
    1212는 미완성.

---
+++ 1212.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string n;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	reverse(n.begin(),n.end());
+	int len = 0;
+	for(int i=0;i<n.size();i++){
+		int temp = n[i];
+
+	}
+	return 0;
+}
