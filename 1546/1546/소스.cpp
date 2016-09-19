4ee46af1d472bc127c4a3af159e414f5eba9dcb1
jjunCoder <jjune21@gmail.com>
2016-09-19 15:32:19 +0900

    평균 solve

---
+++ 1546/1546/소스.cpp
@@ -0,0 +1,39 @@
+﻿/*
+2016.9.19
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1546
+평균
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n, score;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	vector<double> v;
+	for (int i = 0; i < n; i++) {
+		cin >> score;
+		v.push_back(score);
+	}
+	sort(v.begin(), v.end());
+	double maxi = v.back();
+	for (int i = 0; i < n; i++) {
+		v[i] = (v[i] / maxi) * 100;
+	}
+	double sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += v[i];
+	}
+	printf("%.2f", sum / n);
+	return 0;
+}
