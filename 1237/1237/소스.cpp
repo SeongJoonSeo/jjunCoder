d30beb26af1bd638235f75cd4a7b364fe897d219
jjunCoder <jjune21@gmail.com>
2016-09-19 13:25:47 +0900

    정ㅋ벅ㅋ, 단어 나누기 solve - 복습중

---
+++ 1237/1237/소스.cpp
@@ -0,0 +1,20 @@
+﻿/*
+2016.9.19
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1237
+정ㅋ벅ㅋ
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	printf("문제의 정답");
+	return 0;
+}
---
+++ 1251/1251/소스.cpp
@@ -0,0 +1,37 @@
+﻿/*
+2016.9.19
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1251
+단어 나누기
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	string input;
+	cin.sync_with_stdio(false);
+	cin >> input;
+	vector<string> v;
+	for (int i = 0; i < input.size() - 2; i++) {
+		for (int j = i + 1; j < input.size() - 1; j++) {
+			string temp = input;
+			reverse(temp.begin(), temp.begin() + i + 1);
+			reverse(temp.begin() + i + 1, temp.begin() + j + 1);
+			reverse(temp.begin() + j + 1, temp.end());
+			v.push_back(temp);
+		}
+	}
+	sort(v.begin(), v.end());
+	cout << v.front();
+	return 0;
+}
