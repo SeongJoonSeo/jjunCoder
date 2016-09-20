7b16d56e7dbc8835b7142d57c12a3d03732293ee
jjunCoder <jjune21@gmail.com>
2016-09-20 17:59:49 +0900

    복습..! solve

---
+++ 5597/5597/소스.cpp
@@ -0,0 +1,34 @@
+﻿/*
+2016.9.20
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#5597
+과제 안 내신 분..?
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<bool> v;
+	v.push_back(false);
+	for (int i = 0; i < 30; i++) {
+		v.push_back(false);
+	}
+	int n;
+	for (int i = 0; i < 28; i++) {
+		cin >> n;
+		v[n] = true;
+	}
+	
+	for (int i = 1; i <= 30; i++) {
+		if (!v[i])
+			cout << i << endl;
+	}
+	return 0;
+}
