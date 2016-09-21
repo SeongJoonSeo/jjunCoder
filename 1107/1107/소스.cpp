c75e4494c7eb5a74cf4d55ccea3cf1c2fe8735fc
jjunCoder <jjune21@gmail.com>
2016-09-21 17:48:03 +0900

    1107 리모콘 많이 틀림... 나중에 고치자.

--- 1107/1107/소스.cpp
+++ 1107/1107/소스.cpp
@@ -57,15 +57,21 @@ int main() {
 	if (allSafe) {
 		cout << numSize; return 0;
 	}
+	//버튼이 모두 고장나거나 0만 남았을 떄.
+	if (buttons.empty() || (buttons.size()==1 && v[0])) {
+		//+,-로 가야 함
+		cout << diff; return 0;
+	}
 	//둘을 섞어야 최소인 경우.
 	int idx = 0;
 	bool up = true; //채널을 위로 올라가야 할지, 내려가야할지.
 	bool first = true;//맨 윗자리일때만 up을 바꾸어야 한다.
 	while (idx != temp.size()) {
 		int x = temp[idx] - 48;		
-		if (!v[x]) {
+		if (!v[x] && first) {
 			//temp 의 idx번째 자리가 x고, 그 버튼이 고장났을때.
-			int cnt = 0, left = x - 1, right = x + 1, cnt2 = 0;
+			int cnt = 0, cnt2 = 0;
+			int left = x - 1, right = x + 1;
 			//x의 가장 가까운 수 중 고장나지 않은 버튼을 찾는다.
 			while (left >= 0 && !v[left]) {
 				cnt++; left--;
@@ -73,54 +79,43 @@ int main() {
 			while (right != 10 && !v[right]) {
 				cnt2++; right++;
 			}
-
-			//모두 고장났거나 0만남았을경우
-			if (left <= 0 && right == 10) {
-				//+,-로 가야 함
-				cout << diff; return 0;
+			if (right == 10 && left != -1) {
+				//위쪽 번호대만 고장났을때
+				//아래쪽 번호대로 바꾸고 ,				down이다.
+				temp[idx] = left + 48;
+				if (first)
+					up = false;//down으로 바꿈.
 			}
-			else {				
-				if (right == 10 && left != -1) {
-					//위쪽 번호대만 고장났을때
-					temp[idx] = left + 48;					
-				}
-				else if (left == -1 && right != 10) {
-					//아래쪽 번호대만 고장났을때
+			else if (left == -1 && right != 10) {
+				//아래쪽 번호대만 고장났을때
+				//위쪽 번호대로 바꾸고,				up이다.
+				temp[idx] = right + 48;				
+			}
+			else {
+				//둘다 있을때
+				//temp 의 idx번째 자리의 수를 고장나지 않은 버튼으로 바꾼다.				
+				//cnt가 더 크면 채널을 크게 잡아서 내려가는게 더 낫다.
+				if (cnt > cnt2) {
+					up = false;					//down이다.
 					temp[idx] = right + 48;
-					if (first)
-						up = false;
 				}
-				else {
-					//둘다 있을때
-					//temp 의 idx번째 자리의 수를 고장나지 않은 버튼으로 바꾼다.
-					if (first) {//처음 들어온거면
-						//cnt가 더 크면 채널을 크게 잡아서 내려가는게 더 낫다.
-						if (cnt > cnt2) {
-							up = false;
-							temp[idx] = right + 48;
-						}
-						else {
-							temp[idx] = left + 48;
-						}						
-					}
-					else {
-						if (up) {//채널을 올라가야한다.
-							//채널을 높게잡자.
-							temp[idx] = right + 48;
-						}
-						else {//채널을 내려가야한다.
-							//채널을 낮게잡자.
-							temp[idx] = left + 48;
-						}
-					}
-					
-				}	
-
-				
+				else {							//up이다.
+					temp[idx] = left + 48;
+				}
 			}
+			first = false;
 		}
-		idx++;
-		first = false;
+		else if (!v[x]) {
+			if (up) {//채널을 올라가야한다.
+					 //채널을 높게잡자.	
+				temp[idx] = buttons.back() + 48;
+			}
+			else {//채널을 내려가야한다.
+				  //채널을 낮게잡자			  
+				temp[idx] = buttons.front() + 48;
+			}
+		}		
+		idx++;		
 	}
 	//가장 최소로 움직일 수 있는 채널로 옮겼다.
 	//버튼 누른 수 + 옮긴 채널과 현재 채널의 차이를 출력하자.
