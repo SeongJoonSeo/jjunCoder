91cb877d9fc0cc6a8f000c77c9c212062a44ee3c
jjunCoder <jjune21@gmail.com>
2016-09-22 17:10:48 +0900

    1306 달려라 홍준 unsolve...

---
+++ 1297/1297/소스.cpp
@@ -0,0 +1,26 @@
+﻿/*
+2016.9.22
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1297
+TV 크기
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	double a, h, w;
+	cin.sync_with_stdio(false);
+	cin >> a >> h >> w;
+	double k;
+	k = sqrt(a*a / (h*h + w*w));
+	printf("%d %d", (int)floor(h*k), (int)floor(w*k));
+	return 0;
+}
---
+++ 1306/1306/소스.cpp
@@ -0,0 +1,54 @@
+﻿/*
+2016.9.22
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1306
+달려라 홍준
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <deque>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	cin.sync_with_stdio(false);
+	cin >> n >> m;
+	int light,maxi = -1;
+	int s;
+	if (2 * m - 1 <= n)
+		s = 2 * m - 1;
+	else
+		s = n;
+	deque<int> deq;
+	for (int i = 0; i < s ; i++) {
+		cin >> light;		
+		deq.push_back(light);
+	}
+	auto it = max_element(deq.begin(), deq.end());	
+	printf("%d ", *it);
+	while (cin >> light) {
+		if (deq.begin() == it) {
+			deq.pop_front();
+			deq.push_back(light);
+			it = max_element(deq.begin(), deq.end());			
+		}
+		else {
+			if (*it < light)
+				it = deq.end() - 1;
+			deq.pop_front();
+			
+			deq.push_back(light);
+		}
+			
+		printf("%d ", *it);
+	}
+
+	return 0;
+}
