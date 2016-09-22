8c890575a3c4be1a1b0063e8db9830f9d6f24251
jjunCoder <jjune21@gmail.com>
2016-09-22 13:33:22 +0900

    1275번 커피숍은 나중에 푸는걸로....아직은 부족하다.

---
+++ 1225/1225/소스.cpp
@@ -0,0 +1,33 @@
+﻿/*
+2016.9.22
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1225
+이상한 곱셈
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string a, b;
+	cin.sync_with_stdio(false);
+	cin >> a >> b;
+	long long x, y, sum = 0;
+
+	for (int i = 0; i < a.size(); i++) {
+		for (int j = 0; j < b.size(); j++) {
+			x = a[i] - 48;
+			y = b[j] - 48;
+			sum += x*y;
+		}
+	}
+	cout << sum;
+	return 0;
+}
---
+++ 1275/1275/소스.cpp
@@ -0,0 +1,47 @@
+﻿/*
+2016.9.22
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1275
+커피숍2
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n, q, k;
+	cin.sync_with_stdio(false);
+	cin >> n >> q;
+	vector<int> v;
+	for (int i = 0; i < n; i++) {
+		cin >> k;
+		v.push_back(k);
+	}
+	vector<long long> psum(n,0);
+	vector<pair<int, int> > diff;
+	psum[0] = v[0];
+	for (int i = 1; i < n; i++)
+		psum[i] = psum[i - 1] + v[i];
+
+	int x, y, a, b;
+	for (int i = 0; i < q; i++) {
+		cin >> x >> y >> a >> b;
+		
+		x--; y--;
+		if (x != 0) {
+			printf("%lld\n", psum[y] - psum[x - 1]);
+		}
+		else {
+			printf("%lld\n", psum[y]);
+		}
+		
+	}
+	return 0;
+}
