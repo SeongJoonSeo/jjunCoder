75f35186790b941ca570b1f08fa0978eafc8e962
jjunCoder <jjune21@gmail.com>
2016-09-27 13:54:50 +0900

    Fraud Busters 꿀문제 solve. 9465 스티커 문제 unsolve. 미완성

---
+++ 9465/9465/소스.cpp
@@ -0,0 +1,36 @@
+﻿/*
+2016.9.27
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#9465
+스티커
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int t, n, input;
+	cin.sync_with_stdio(false);
+	cin >> t;
+	while (t--) {
+		cin >> n;
+		vector<vector<int> > board;
+		board.assign(n, vector<int>(2, 0));
+		for (int j = 0; j < 2; j++) {
+			for (int i = 0; i < n; i++) {
+				cin >> input;
+				board[j][i] = input;
+			}
+		}
+		
+	}
+	return 0;
+}
---
+++ 9584/9584/소스.cpp
@@ -0,0 +1,41 @@
+﻿/*
+2016.9.27
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#9584
+Fraud Busters
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	string str, input;
+	int n;
+	vector<string> v;
+	cin.sync_with_stdio(false);
+	cin >> str >> n;
+	for (int i = 0; i < n; i++) {
+		bool correct = true;
+		cin >> input;
+		for (int j = 0; j < input.size(); j++) {
+			if (str[j] == input[j] || str[j] == '*')
+				continue;
+			correct = false;
+		}
+		if (correct)
+			v.push_back(input);
+	}
+	printf("%d\n", v.size());
+	for (int i = 0; i < v.size(); i++)
+		printf("%s\n", v[i].c_str());
+	return 0;
+}
