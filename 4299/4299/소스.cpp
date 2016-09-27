6d113f1a5543cbca2c1461afe72b7b599255de95
jjunCoder <jjune21@gmail.com>
2016-09-27 18:00:09 +0900

    AFC윔블던 솔브.

---
+++ 4299/4299/소스.cpp
@@ -0,0 +1,33 @@
+﻿/*
+2016.9.27
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#4299
+AFC 윔블던
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	int sum, sub;
+	cin.sync_with_stdio(false);
+	cin >> sum >> sub;
+	if ((sum + sub) % 2 != 0)
+		cout << -1;
+	else if (sum < sub)
+		cout << -1;
+	else {
+		a = (sum + sub) / 2;
+		b = sum - a;
+		printf("%d %d", a, b);
+	}
+
+	return 0;
+}
