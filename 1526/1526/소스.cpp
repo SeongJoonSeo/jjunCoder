5a807dc271c3cb313eaf7ec4dc5db6d23943ac21
jjunCoder <jjune21@gmail.com>
2016-09-27 14:21:05 +0900

    가장 큰 금민수 unsolve.

---
+++ 1526/1526/소스.cpp
@@ -0,0 +1,24 @@
+﻿/*
+2016.9.27
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1526
+가장 큰 금민수
+*/
+
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string n;
+	cin.sync_with_stdio(false);
+	cin >> n;
+}
