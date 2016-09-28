d6ffdc4130f2829041c0c1d077cc416cf1bc9c7e
jjunCoder <jjune21@gmail.com>
2016-09-28 09:55:28 +0900

    랜선자르기 미완성

---
+++ 1654/1654/소스.cpp
@@ -0,0 +1,28 @@
+﻿/*
+2016.9.28
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1654
+랜선 자르기
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int k, n, input;
+	cin.sync_with_stdio(false);
+	cin >> k >> n;
+	vector<long long> v;
+	for (int i = 0; i < k; i++) {
+		cin >> input;
+		v.push_back(input);
+	}
+	return 0;
+}
