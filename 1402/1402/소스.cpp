12faa81d495b65f032dcfc85b99d16162f0a48aa
jjunCoder <jjune21@gmail.com>
2016-09-28 17:27:40 +0900

    아무래도이문제는A번난이도인것같다 unsolve.

---
+++ 1402/1402/소스.cpp
@@ -0,0 +1,27 @@
+﻿/*
+2016.9.28
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1402
+아무래도이문제는A번난이도인것같다
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin.sync_with_stdio(false);
+	cin >> t;
+	while (t--) {
+		int a, b;
+		cin >> a >> b;
+
+	}
+	return 0;
+}
