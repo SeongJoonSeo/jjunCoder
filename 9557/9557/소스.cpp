9ac0cdefe3376936dd046abca5bf411d82d6acf3
jjunCoder <jjune21@gmail.com>
2016-09-28 17:54:54 +0900

    Arabic and English 미완성.

---
+++ 9557/9557/소스.cpp
@@ -0,0 +1,36 @@
+﻿/*
+2016.9.28
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#9557
+Arabic and English
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin.sync_with_stdio(false);
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		string input, appending;
+		bool arabic = false;
+		bool alpha = false;
+		bool canSwap = false;
+		for (int i = 0; i < n; i++) {			
+			cin >> input;
+			if (input.front() == '#')
+				arabic = true;
+		}
+	}
+	return 0;
+}
