243609d59a12e7f81dbb9cb1592d8404888abecf
jjunCoder <jjune21@gmail.com>
2016-09-29 13:19:10 +0900

    괄호 - stack을 이용한다. solve

---
+++ 9012/9012/소스.cpp
@@ -0,0 +1,55 @@
+﻿/*
+2016.9.29
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#9012
+괄호
+*/
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+bool isVPS(string input) {
+	stack<char> s;
+	auto it = input.begin();
+	s.push(*(it++));
+	while (it != input.end()) {
+		if (*it == '(') {
+			s.push(*(it++));
+		}
+		else {
+			if (s.empty())
+				return false;
+			if (s.top() != '(')
+				return false;
+			else
+				s.pop(); it++;
+		}
+	}
+	if (!s.empty())
+		return false;
+	else
+		return true;
+}
+
+int main() {
+	int t;
+	cin.sync_with_stdio(false);
+	cin >> t;
+	while (t--) {
+		string input;
+		cin >> input;
+		if (isVPS(input))
+			printf("YES\n");
+		else
+			printf("NO\n");
+	}
+	return 0;
+}
