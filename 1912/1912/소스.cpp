0b99ad75e4eecb8bf45651501d6a57420eea3c74
jjunCoder <jjune21@gmail.com>
2016-09-30 17:52:31 +0900

    연속합에 대해서 공부하자!

---
+++ 1912/1912/소스.cpp
@@ -0,0 +1,33 @@
+﻿/*
+2016.9.29
+BaekJoon Online Judge
+Problem Solving
+
+Seong Joon Seo (ID: jjunCoder)
+
+Problem
+#1912
+연속합
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+const int INF = 987654321;
+
+int main() {
+	int n, input;
+	cin.sync_with_stdio(false);
+	cin >> n;
+	vector<vector<int>> dp;
+	vector<int> v;
+
+	dp.assign(n, vector<int>(n, INF));
+	for (int i = 0; i < n; i++) {
+		cin >> input;
+		v.push_back(input);
+	}
+	return 0;
+}
