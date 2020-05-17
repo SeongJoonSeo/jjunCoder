bdb97a77db3c75559115f5a23ac66cdebd528f26
jjuncoder <jjune21@gmail.com>
2020-05-17 12:00:47 +0900

    Update Solve.sh script for title

---
+++ 2156/2156.cpp
@@ -0,0 +1,15 @@
+// BOJ 2156번: 포도주 시식
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}
--- solve.sh
+++ solve.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Problem Solving Start!"
-mkdir $1 && cd $1 && touch $1.cpp && code $1.cpp
+mkdir $1 && cd $1 && echo "// BOJ $1번: $2" >> $1.cpp && code $1.cpp
